bb09aa486e7c1f787ae69bd257c18f4e48695f66
Eneko Alonso <eneko.alonso@gmail.com>
2019-06-21 08:04:19 -0700

    Cleanup

--- Sources/ConsoleUI/MySwiftUIView.swift
+++ Sources/ConsoleUI/MySwiftUIView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct MySwiftUIView : View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/).color(.red)
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+            Text("Hello World!").color(.red).font(.largeTitle)
+        }
     }
 }
 
--- Sources/ConsoleUI/main.swift
+++ Sources/ConsoleUI/main.swift
@@ -2,13 +2,6 @@
 import AppKit
 import SwiftUI
 
-let frame = CGRect(x: 0, y: 0, width: 1000, height: 1000)
-
-let view = MySwiftUIView()
-let wrapper = NSHostingView(rootView: view)
-wrapper.frame = frame
-print(wrapper.frame)
-
 func rasterize(view: NSView, format: NSBitmapImageRep.FileType) -> Data? {
     guard let bitmapRepresentation = view.bitmapImageRepForCachingDisplay(in: view.bounds) else {
         return nil
@@ -17,6 +10,10 @@ func rasterize(view: NSView, format: NSBitmapImageRep.FileType) -> Data? {
     view.cacheDisplay(in: view.bounds, to: bitmapRepresentation)
     return bitmapRepresentation.representation(using: format, properties: [:])
 }
+
+
+let wrapper = NSHostingView(rootView: MySwiftUIView())
+wrapper.frame = CGRect(x: 0, y: 0, width: 800, height: 450)
 
 let png = rasterize(view: wrapper, format: .png)
 try png?.write(to: URL(fileURLWithPath: "test.png"))
